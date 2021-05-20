efbb8179f2349b083f263c6d8e32d6d96ee51440
Stepan8888 <58593274+Stepan8888@users.noreply.github.com>
2021-05-20 13:53:52 +0300

    Get address wallet balance and dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
-use iota_client::Client;
+use iota_client::{Client};
 
 #[tokio::main]
 async fn main() {
-    let iota = Client::builder() // Crate a client instance builder
-        .with_node("https://api.lb-0.testnet.chrysalis2.com")
-        .unwrap()
-        .finish()
-        .await
-        .unwrap();
-
-    let info = iota.get_info().await.unwrap();
-    println!("Nodeinfo: {:?}", info);
+    let balance = Balance{ };
+    balance.balance().await;
+}
+struct Balance {}
+impl Balance {
+    async fn balance(&self)
+    {
+        let iota = Client::builder()
+            .with_node("https://chrysalis-nodes.iota.org")
+            .unwrap()
+            .with_node_sync_disabled()
+            .finish()
+            .await
+            .unwrap();
+        let address = "iota1qrepert94y80wyx6j07n2zgldfy2509hpugkx3apvdga0uux780xz3xtcyf";
+        let response = iota.get_address().balance(&address).await.unwrap();
+        let balance: u64 = response.balance;
+        #[derive(Debug)]
+        struct Information {
+            balance: u64,
+            address: String
+        }
+        let balance_address = Information { balance, address: response.address };
+        println!("{:?}", balance_address)
+    }
 }
